d168e463b97db14955e9e3f803a0c5ed3a5e979a
Spencer Rohan <spencermrohan@gmail.com>
2015-12-03 11:11:14 -0600

    ADD watch gulp task and create main less stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ var gulp   = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('process-less', function(){
-  gulp.src(['src/less/import.less'])
+  gulp.src(['src/less/import.less',
+            'src/less/site.less'])
     .pipe(less())
-    .pipe(concat('app.css'))
+    .pipe(concat('site.css'))
     .pipe(gulp.dest('assets/css'))
 });
 
 gulp.task('process-scripts', function(){
-  gulp.src('src/js/*.js')
-    .pipe(concat('app.js'))
+  gulp.src(['src/vendor/jquery/dist/jquery.min.js',
+            'src/vendor/bootstrap/dist/js/bootstrap.min.js',
+            'src/js/*.js'])
+    .pipe(concat('site.js'))
     .pipe(gulp.dest('assets/js'))
 });
+
+gulp.task('watch', function(){
+  gulp.watch('src/less/site.less', ['process-less'])
+});
+
